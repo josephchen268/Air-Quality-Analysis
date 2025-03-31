3ba77aef5a2fdc7814aa693ed88f51415b7306c2
joe <117794383+jochen182@users.noreply.github.com>
2025-03-30 19:17:22 -0700

    Created Subset of data, will update more tomorrow.

--- Air_quality.py
+++ Air_quality.py
@@ -1,3 +1,9 @@
 import pandas as pd
+
+# put Air Quality Data into Dataframe
 df = pd.read_csv('Air_Quality.csv')
-print(df.head(2))
+
+# Remove and filter columns and rows that we are not using for this analysis
+cols = df[["Unique ID", "Name", 'Measure',"Measure Info", "Geo Place Name", "Start_Date", "Data Value"]]
+PM_df = cols[cols["Name"] == "Fine particles (PM 2.5)"]
+print(PM_df.head())
